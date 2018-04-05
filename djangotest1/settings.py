a9fa1c3987d6b39631fb979ef32903a9630a1ec5
eric-dl <31330964+eric-dl@users.noreply.github.com>
2018-04-04 21:02:31 -0500

    4/4

--- djangotest1/settings.py
+++ djangotest1/settings.py
@@ -31,12 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'guessbook',
+    'hello',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
